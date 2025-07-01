7b7392b716289ab05a6b04519acac0d16120d50f
Aarav <104804813+AaravBansal@users.noreply.github.com>
2025-07-01 17:13:13 +1200

    this was the beginning of me starting to work on the front end using a tutorial of MUI which is the leading framwork for all things to do with REACT. Big companies use this same framework so I thought it would be easier for me to do the same while still coding all of it.

--- web/open-banking/src/App.js
+++ web/open-banking/src/App.js
@@ -1,60 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 function App() {
-
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log('Username:', username);
-      console.log('Password:', password);
-    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="login-container">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="username">Username:</label>
-                <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-              </div>
-              <button type="submit">Login</button>
-            </form>
-          </div>
-        </a>
-      </header>
-    </div>
-  );
+
+  )
+
 }
 
 export default App;
