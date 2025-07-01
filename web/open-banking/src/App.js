d8ea4b6194dfc8be712261fdd975cf855f3c5205
Aarav <104804813+AaravBansal@users.noreply.github.com>
2025-07-01 18:04:11 +1200

    This was the end of the tutorial that was going to work the best for me. I have now got a very simple login page created that looks good, but only has basic colors such as white and blue for the sign in button. I will now work on fixing this as well as getting this login page linked up with my backend login page.

--- web/open-banking/src/App.js
+++ web/open-banking/src/App.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import {
-  Box,
-  Button,
-  Container,
-  Paper,
-  TextField,
-  Typography,
+    Container,
+    Paper,
+    Typography,
+    Box,
+    TextField,
+    Button,
 } from '@mui/material';
 
-function App() {
-  return (
-
-  )
-
+export default function LoginPage() {
+    return (
+        <Container component="main" maxWidth="xs">
+            <Paper
+                elevation={6}
+                sx={{
+                    mt: 8,
+                    p: 4,
+                    borderRadius: 3,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="h5" gutterBottom>
+                    Welcome to Open Banking
+                </Typography>
+                <Box component="form" noValidate sx={{ mt: 2, width: '100%' }}>
+                    <TextField
+                        fullWidth
+                        label="Username"
+                        margin="normal"
+                        required
+                    />
+                    <TextField
+                        fullWidth
+                        label="Password"
+                        margin="normal"
+                        type="password"
+                        required
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3 }}
+                    >
+                        Sign In
+                    </Button>
+                </Box>
+            </Paper>
+        </Container>
+    );
 }
-
-export default App;
